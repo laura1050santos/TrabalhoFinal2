d21d490ca4a1e5e49bd18b52d914d17eb14cb914
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2024-12-19 22:58:41 -0300

    arrumei rotas campeao
    arrumei rotas campeao

--- TrabalhoFinal/controller/campeoes.py
+++ TrabalhoFinal/controller/campeoes.py
@@ -41,15 +41,15 @@ def update_campeao(id_campeao):
         nome = request.form.get('nome')
         dificuldade = request.form.get('dificuldade')
         campeoes_repository.update_campeao(id_campeao,nome,dificuldade) #chama a função do repository que 
-        return redirect(url_for('campeao.update_campeoes'))
+        return redirect(url_for('campeao.ver_campeoes'))
     return render_template('/campeao/update_campeao.html')
 
 
 @campeoesController.route('/delete_campeao',methods=['DELETE', 'GET'])
-def delete_campeao(id_campeao):
+def delete_campeao():
     if request.method == 'DELETE':
         id_campeao = request.form.get('id_campeao')
-        campeao = campeoes_repository.get_campeao_by_id(id)
+        campeao = campeoes_repository.get_campeao_by_id(id_campeao=id_campeao)
         campeoes_repository.delete_campeao(campeao)
-        return redirect(url_for('campeao.delete_campeoes'))
+        return redirect(url_for('campeao.ver_campeoes'))
     return render_template('/campeao/delete_campeao.html')
